2b9f8393df0b26fbcf02958bb309e63232ccb2ba
drkwitht <derektan3510@gmail.com>
2023-09-10 23:51:52 -0700

    Implemented hashing helper.

--- include/utils/myhash.h
+++ include/utils/myhash.h
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #define MY_HASH_PRIME 3
+#define MY_HASH_LIMIT 10
 
 size_t hash_cstr(const char *str);
 
---
+++ src/myhash.c
@@ -0,0 +1,26 @@
+/**
+ * @file myhash.c
+ * @author Derek Tan
+ * @brief Implements a simple hash function.
+ * @date 2023-09-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "utils/myhash.h"
+
+size_t hash_cstr(const char *str)
+{
+    size_t len = strnlen(str, MY_HASH_LIMIT);
+    size_t base = 1;
+    size_t hash = 0;
+
+    for (size_t i = 0; i < len; i++)
+    {
+        hash += base * str[i];
+        base *= MY_HASH_PRIME;
+    }
+
+    return hash;
+}
